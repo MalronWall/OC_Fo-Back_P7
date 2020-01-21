ff055bd6b7fc7b835186f0cc5faebba8898ef9ef
MalronWall <thibaut.tourte17@gmail.com>
2020-01-21 22:32:57 +0100

    [FEAT] add url in JWT errors

--- src/AppBundle/Listeners/Security/ApiListener.php
+++ src/AppBundle/Listeners/Security/ApiListener.php
@@ -12,6 +12,7 @@ use AppBundle\Domain\Helpers\Security\JWTManager;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class ApiListener
 {
@@ -23,18 +24,25 @@ class ApiListener
      * @var ContainerInterface
      */
     private $container;
+    /**
+     * @var UrlGeneratorInterface
+     */
+    private $urlGenerator;
 
     /**
      * ApiListener constructor.
      * @param JWTManager $JWTManager
      * @param ContainerInterface $container
+     * @param UrlGeneratorInterface $urlGenerator
      */
     public function __construct(
         JWTManager $JWTManager,
-        ContainerInterface $container
+        ContainerInterface $container,
+        UrlGeneratorInterface $urlGenerator
     ) {
         $this->JWTManager = $JWTManager;
         $this->container = $container;
+        $this->urlGenerator = $urlGenerator;
     }
 
     /**
@@ -42,18 +50,20 @@ class ApiListener
      */
     public function onKernelRequest(GetResponseEvent $event)
     {
-        if (preg_match("/\/login/", $event->getRequest()->getRequestUri())) {
+        if (preg_match("/\/login/", $event->getRequest()
+                                          ->getRequestUri())) {
             return;
         }
 
         $jwt = $event->getRequest()->headers->get("Authorization");
 
+        $msg["errors"]["loginUrl"] = $this->urlGenerator->generate("login");
+
         if (is_null($jwt)) {
+            $msg["errors"]["errorMessage"] = "Bearer Authorization Token required";
             $event->setResponse(
                 new Response(
-                    json_encode(
-                        ["errors" => "Bearer Authorization Token required"]
-                    ),
+                    json_encode($msg),
                     Response::HTTP_UNAUTHORIZED,
                     [
                         'Content-Type' => 'application/json'
@@ -66,11 +76,10 @@ class ApiListener
         $jwt = $this->JWTManager->buildWithString($jwt);
 
         if (!$jwt->isValid($this->container->getParameter("jwt_secret"))) {
+            $msg["errors"]["errorMessage"] = "Invalid JWT";
             $event->setResponse(
                 new Response(
-                    json_encode(
-                        ["errors" => "Invalid JWT"]
-                    ),
+                    json_encode($msg),
                     Response::HTTP_UNAUTHORIZED,
                     [
                         'Content-Type' => 'application/json'
@@ -81,11 +90,10 @@ class ApiListener
         }
 
         if (!$jwt->isOnDate()) {
+            $msg["errors"]["errorMessage"] = "JWT expired";
             $event->setResponse(
                 new Response(
-                    json_encode(
-                        ["errors" => "JWT expired"]
-                    ),
+                    json_encode($msg),
                     Response::HTTP_UNAUTHORIZED,
                     [
                         'Content-Type' => 'application/json'
@@ -96,11 +104,10 @@ class ApiListener
         }
 
         if (!$this->JWTManager->isValidClient($jwt->getPayload())) {
+            $msg["errors"]["errorMessage"] = "Invalid User JWT";
             $event->setResponse(
                 new Response(
-                    json_encode(
-                        ["errors" => "Invalid User JWT"]
-                    ),
+                    json_encode($msg),
                     Response::HTTP_UNAUTHORIZED,
                     [
                         'Content-Type' => 'application/json'
@@ -110,7 +117,9 @@ class ApiListener
             return;
         }
 
-        $event->getRequest()->getSession()->set('JWT', $jwt->getPayload()->sub);
+        $event->getRequest()
+              ->getSession()
+              ->set('JWT', $jwt->getPayload()->sub);
 
         return;
     }
